95c1234291c6b4423bcabcdfd28dd250d4bbda79
Paul Tynell <paul.tynell@gmail.com>
2023-04-24 19:36:30 +0300

    added comments to app.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+// Import the necessary modules
 import React, { useState } from "react";
 import "./app.css";
 import RefuelingForm from "./components/RefuelingForm";
@@ -5,20 +6,28 @@ import RefuelingList from "./components/RefuelingList";
 import RefuelingStats from "./components/RefuelingStats";
 
 function App() {
+  // Declare state for the refueling list using the useState hook
   const [refuelingList, setRefuelingList] = useState([]);
 
+  // Function to add a new refueling expense to the refueling list
   const addRefuelingExpense = (newRefueling) => {
+    // Update the refueling list with the new refueling expense
     setRefuelingList([...refuelingList, newRefueling]);
   };
 
   return (
     <div className="App">
       <h1>Car Fuel Expenses</h1>
+      // Render the RefuelingForm component and pass the addRefuelingExpense function as a prop
       <RefuelingForm onAddRefuelingExpense={addRefuelingExpense} />
+      // Render the RefuelingList component and pass the refueling list as a prop
       <RefuelingList refuelingList={refuelingList} />
+      // Render the RefuelingStats component and pass the refueling list as a prop
       <RefuelingStats refuelingList={refuelingList} />
     </div>
   );
 }
 
+// Export the App component as the default export
 export default App;
+
